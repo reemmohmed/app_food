7c15eea7da6f65daafae0423faa1e688c7a67d19
reemmohmed <reemmohmedmohmed@gmail.com>
2025-10-19 12:27:00 +0300

    app_ food

---
+++ lib/features/splash/splash_view.dart
@@ -0,0 +1,25 @@
+import 'package:app_food/core/constants/app_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.primary,
+      body: Center(
+        child: Column(
+          children: [
+            Gap(290),
+            SvgPicture.asset("assets/splash/Hungry_.svg"),
+            Spacer(),
+            Image.asset("assets/splash/image 1.png"),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:app_food/features/splash/splash_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppFood());
+}
+
+class AppFood extends StatelessWidget {
+  const AppFood({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AppFood',
+      home: SplashView(),
+    );
+  }
+}
